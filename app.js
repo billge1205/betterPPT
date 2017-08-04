27292f60acc9995053881dd055ed7c54981357f2
billge1205 <billge_1205@sina.com>
2017-08-04 20:28:30 +0800

    fix

--- app.js
+++ app.js
@@ -13,9 +13,19 @@ http.createServer(function (req, res) {
             res.write(fs.readFileSync('views/ppt.html', 'utf-8').toString());
             res.end();
             break;
+        case '/node/wejs':
+            res.writeHead(200, { 'Content-Type': 'text/html'});
+            res.write(fs.readFileSync('views/wejs.html', 'utf-8').toString());
+            res.end();
+            break;
+        case '/node/demo':
+            res.writeHead(200, { 'Content-Type': 'text/html'});
+            res.write(fs.readFileSync('views/demo.html', 'utf-8').toString());
+            res.end();
+            break;
         default:
             res.writeHead(404, { 'Content-Type': 'text/plain' }); res.end('Page Not Found\n');
             break;
     }
 }).listen(8080, "127.0.0.1");
-console.log('Server running at http://127.0.0.1:8080/');
+console.log('Server running at http://127.0.0.1/');
---
+++ js/demo.js
@@ -0,0 +1,6 @@
+/**
+ * Created by billge on 17/8/4.
+ */
+define(function (module) {
+    module.exports.version = '1.0.1';
+});
